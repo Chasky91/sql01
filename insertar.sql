12e18cdc155067752bd43eb88e65726edb862c42
Chasky91 <david_c91@outlook.com>
2024-09-13 22:58:14 -0300

    insercion de datos

---
+++ insertar.sql
@@ -0,0 +1,8 @@
+INSERT INTO ventas (
+    nombre_producto, 
+    cantidad, precio_total,
+    fecha_venta
+) VALUES 
+('Café Latte', 1, 4.00, '2024-09-09'), 
+('Té Verde', 2, 7.00, '2024-09-08'), 
+('Muffin de Arándanos', 4, 10.00, '2024-09-09');
