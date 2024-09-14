1cc5fb539b728969988ae4a0f27463c18fad1d5f
Chasky91 <david_c91@outlook.com>
2024-09-13 23:12:07 -0300

    si¡uministros

---
+++ suministros.sql
@@ -0,0 +1,5 @@
+CREATE TABLE suministros ( 
+id_suministro INT AUTO_INCREMENT PRIMARY KEY, nombre_producto VARCHAR(100) NOT NULL, 
+cantidad INT NOT NULL, 
+precio_unitario DECIMAL(10,2) NOT NULL
+); 
